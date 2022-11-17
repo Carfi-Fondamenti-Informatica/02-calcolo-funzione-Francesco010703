70ee66272658d2193dc4fabe16e9b5daa2e7750d
Francesco010703 <118008638+Francesco010703@users.noreply.github.com>
2022-11-17 17:40:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
+   float a, b, x, y, z;       //inizializzo le variabili
+   cin >> a >> b >> x >> y;   //l'utente inserisce i valori delle variabili
+   
+   if (x<0 && y>0) {
+      z = (a*x) - (b*y);      //calcolo la funzione nei 3 casi
+   }
+   
+   else if (x>=0 && y<=0) {
+      z = (a*x*x) - (b*y);
+   }
+   
+   else {
+      z = (a*x) + (b*y*y);
+   }
+   
+   cout << z << endl;      // stampo a schermo il risultato
+   
    return 0;
 }
